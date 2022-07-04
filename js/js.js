967eb807fb7d8da74bbc8f47c9fe5b48c999c3b0
Revan4ik <gaetndyuude@gmail.com>
2022-07-04 22:03:55 +0300

    hw-26
    hw-26

---
+++ js/js.js
@@ -0,0 +1,89 @@
+
+const registration = document.querySelector(".reg"),
+registration_button = registration.querySelector("button"),
+registration_input = registration.querySelector("input"),
+todolist = document.querySelector(".todolist"),
+input = todolist.querySelector(".todolist_text"),
+button = todolist.querySelector(".todolist_add"),
+nameuser = todolist.querySelector(".nameofuser"),
+todolist_items = todolist.querySelector(".todolist_items"),
+clearlist = todolist.querySelector(".clearlist")
+
+username = JSON.parse(sessionStorage.getItem('username'))
+
+tasks = JSON.parse(sessionStorage.getItem('tasks'))
+
+if (username) {
+registration.style.visibility = "hidden"
+todolist.style.visibility = "visible"
+}
+
+if (!tasks) {
+tasks = []
+}
+
+tasks.forEach((el) => {
+let li = document.createElement("li"),
+    done = document.createElement("button")
+done.type = "sumbit"
+todolist_items.appendChild(li)
+
+if (el.done) {
+    li.innerHTML = el.content
+    li.className = "donetask"
+} else {
+    li.innerHTML = el.content
+    li.addEventListener("click", function () {
+        li.className = "donetask"
+        el.done = true
+        sessionStorage.setItem("tasks", JSON.stringify(tasks))
+    })
+}
+});
+
+registration_button.addEventListener("click", function () {
+registration.style.visibility = "hidden"
+todolist.style.visibility = "visible"
+
+if (!username) {
+    username = [{ name: `Guest` }]
+}
+})
+
+button.addEventListener("click", function () {
+if (!input.value) {
+    return
+}
+
+tasks.push({
+    content: input.value,
+    done: false,
+})
+
+sessionStorage.setItem('tasks', JSON.stringify(tasks))
+todolist_items.innerHTML = null
+tasks.forEach(el => {
+    let li = document.createElement("li"),
+        done = document.createElement("button")
+    done.type = "sumbit"
+    todolist_items.appendChild(li)
+    if (el.done) {
+        li.innerHTML = el.content
+        li.className = "donetask"
+    } else {
+        li.innerHTML = el.content
+        li.addEventListener("click", function () {
+            li.className = "donetask"
+            el.done = true
+            sessionStorage.setItem("tasks", JSON.stringify(tasks))
+        })
+
+    }
+});
+})
+
+clearlist.addEventListener("click", function () {
+todolist_items.innerHTML = null
+sessionStorage.setItem("tasks", JSON.stringify([]))
+tasks = []
+})
